db6786f0be642c8836f37671d2c7ec3f2b667a59
LF <this.is.my.bot@gmail.com>
2022-03-02 15:31:23 +0100

    proper retry design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TopBarComponent } from './topbar/topbar.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { MovieCardComponent } from './movie-card/movie-card.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopBarComponent,
-    SidebarComponent,
-    MovieCardComponent
+    NavbarComponent,
+
   ],
   imports: [
     BrowserModule,
