ebff1f93906c611a4aa1a238179b7db66d234daa
LF <this.is.my.bot@gmail.com>
2022-03-02 14:31:37 +0100

    header + sidebar menu done

---
+++ src/app/topbar/topbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-topbar',
+  templateUrl: './topbar.component.html',
+  styleUrls: ['./topbar.component.css']
+})
+export class TopBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
